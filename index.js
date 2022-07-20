812c53a78a5afcde706f0c4b3bf9a0a3899839f9
Duy Pham <ericpham19@outlook.com>
2022-07-20 17:07:16 +1000

    Passed tests

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution in this file!
+const employee = {
+    name : "Eric",
+    streetAddress: "12 Cookson" 
+};
+
+
+function updateEmployeeWithKeyAndValue(employee, name, streetAddress){
+    const newEmployee= {...employee}
+    newEmployee.name = "Sam";
+    newEmployee.streetAddress = "11 Broadway";
+     return newEmployee;
+     
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, name, streetAddress){
+    employee.name = "Sam",
+    employee.streetAddress = "12 Broadway"
+    return employee;
+};
+
+
+function deleteFromEmployeeByKey(employee, name) {
+        const cloneEmployee = {... employee};
+
+            cloneEmployee.name = undefined;
+        
+            return cloneEmployee;
+
+
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, name){
+    employee.name = undefined;
+
+    return employee;
+
+}
